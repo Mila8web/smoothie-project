a1013704d283a84148ac4f360d7c23dbd5ee8f35
Mila8web <mila8web@gmail.com>
2021-02-28 20:35:21 +0300

    script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+// Для кнопки GoTop
+// Получить кнопку:
+mybutton = document.getElementById("myBtn");
+// Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
+window.onscroll = function () {
+scrollFunction()
+};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+function topFunction() {
+document.body.scrollTop = 0; // Для Safari
+document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
+
+
+
+// Плавная прокрутка
+let anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault()
+        let blockID = anchor.getAttribute('href')  
+        document.querySelector(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: 'start'
+        })
+    })
+}
